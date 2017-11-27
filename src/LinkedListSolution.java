af701c2852ed5f907f4720beb16ada6b3cb0e4ff
lihaochen <haochenli2000@qq.com>
2017-11-27 18:38:32 +0800

    链表算法题

--- src/LinkedListSolution.java
+++ src/LinkedListSolution.java
@@ -75,26 +75,95 @@ public class LinkedListSolution {
                 temp = temp.next;
             }
             temp.next = tmp;
-
         }
-
 
         for (int i = m; i > 1; i--) {
             head = head.next;
         }
 
 
-//        ListNode originHead = head;
-//        ListNode p = head.next;
-//        for (int i = n; i - m > 0 && p.next != null; i--) {
-//            ListNode q = p.next;
-//            p.next = q.next;
-//            q.next = head.next;
-//            head.next = q;
-//        }
-//        return originHead;
-
         return head;
+    }
+
+
+    /**
+     * You are given two non-empty linked lists representing two non-negative integers.
+     * The digits are stored in reverse order and each of their nodes contain a single digit.
+     * Add the two numbers and return it as a linked list.
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * Output: 7 -> 0 -> 8
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode p = l1;
+        ListNode q = l2;
+        int add = 0;
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (p != null || q != null) {
+            int num1 = (p != null) ? p.val : 0;
+            int num2 = (q != null) ? q.val : 0;
+            int sum = num1 + num2 + add;
+            add = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (p != null) {
+                p = p.next;
+            }
+
+            if (q != null) {
+                q = q.next;
+            }
+
+
+            if (add > 0) {
+                cur.next = new ListNode(add);
+            }
+
+        }
+
+        return head.next;
+//        return addTwoNumbers(l1, l2, 0);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2, int add) {
+
+        if (l1 == null && l2 == null && add == 0) {
+            return null;
+        }
+
+        ListNode l1Next;
+        int num1;
+        if (l1 == null) {
+            l1Next = null;
+            num1 = 0;
+        } else {
+            l1Next = l1.next;
+            num1 = l1.val;
+        }
+
+        ListNode l2Next;
+        int num2;
+        if (l2 == null) {
+            l2Next = null;
+            num2 = 0;
+        } else {
+            l2Next = l2.next;
+            num2 = l2.val;
+        }
+
+
+        int sum = num1 + num2 + add;
+        if (sum >= 10) {
+            add = 1;
+            sum = sum - 10;
+        } else {
+            add = 0;
+        }
+        ListNode node = new ListNode(sum);
+        node.next = addTwoNumbers(l1Next, l2Next, add);
+        return node;
     }
 
 
@@ -109,7 +178,8 @@ public class LinkedListSolution {
 
     public static void main(String[] args) {
 //        reverseBetweenTest();
-        swapPairsTest();
+//        swapPairsTest();
+        addTwoNumbersTest();
     }
 
     private static void swapPairsTest() {
@@ -147,6 +217,34 @@ public class LinkedListSolution {
 
 
         ListNode head = solution.reverseBetween(node1, 2, 4);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    private static void addTwoNumbersTest() {
+//        ListNode a1 = new ListNode(2);
+//        ListNode a2 = new ListNode(4);
+//        ListNode a3 = new ListNode(3);
+//        a1.next = a2;
+//        a2.next = a3;
+
+//        ListNode b1 = new ListNode(5);
+//        ListNode b2 = new ListNode(6);
+//        ListNode b3 = new ListNode(4);
+//        b1.next = b2;
+//        b2.next = b3;
+
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(8);
+        ListNode b1 = new ListNode(0);
+
+        a1.next = a2;
+
+        LinkedListSolution solution = new LinkedListSolution();
+        ListNode head = solution.addTwoNumbers(a1, b1);
+
         while (head != null) {
             System.out.println(head.val);
             head = head.next;
